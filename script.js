7d7d39c6ab8f260f8ae2f727817d083e3e9f5611
Natalija Jolovic <natalijajolovic@gmail.com>
2026-08-26 02:51:50 +0200

    basic movement

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 64 * 16;
-canvas.height = 64 * 9;
+canvas.width = 64 * 16;  // 1024
+canvas.height = 64 * 9;  // 573
 
-c.fillStyle = 'white';
-c.fillRect(0, 0,canvas.width, canvas.height);
-
-c.fillStyle = "red"
-c.fillRect(100, 100, 100, 100);
-
-function animate() {
-    window.requestAnimationFrame(animate);
-    console.log("go");
+const player = new Player();
+const keys = {
+    w : {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    },
+    d: {
+        pressed: false,
+    },
 }
 
+// animation loop
+function animate() {
+    window.requestAnimationFrame(animate);
+    c.fillStyle = 'white';
+    c.fillRect(0, 0,canvas.width, canvas.height);
+
+    player.velocity.x = 0;
+
+    if(keys.d.pressed) {
+        player.velocity.x = 4;
+    } else if(keys.a.pressed) {
+        player.velocity.x = -4;
+    }
+
+    player.draw();
+    player.update();
+}
 animate();
+
